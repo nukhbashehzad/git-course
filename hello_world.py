5ed4f12e6b92ef1a4c3bc6f8169d69ed6e525ab7
nukhba <nukhba.shehzad@tuni.fi>
2026-05-16 22:11:37 +0300

    Fix hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,11 +1,1 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
 print("Hello world!")
-=======
-print("Hello world!")
->>>>>>> intermediate/master
-print("Hello from feature")
-print("Hello from release")
-=======
-print("Hello world!")
->>>>>>> normal-merge/master
